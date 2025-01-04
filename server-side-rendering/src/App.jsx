4c7008d49f27e61a85eeed03888c2cb5423524fe
henriqueAlgauer <algauer.henrique@gmail.com>
2025-01-04 10:49:11 -0300

    testado experimental fetching

--- server-side-rendering/src/App.jsx
+++ server-side-rendering/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
@@ -10,6 +10,7 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: Infinity,
       cacheTime: Infinity,
+      suspense: true
     },
   },
 });
@@ -20,13 +21,19 @@ const App = () => {
     <div>
       <AdoptedPetContext.Provider value={adoptedPet}>
         <QueryClientProvider client={queryClient}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <Suspense fallback={
+            <div className="loading-pane">
+              <h2 className="loader">💿</h2>
+            </div>
+          }>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
         </QueryClientProvider>
       </AdoptedPetContext.Provider>
     </div>
